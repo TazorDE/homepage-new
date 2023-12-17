cf350942e5e4250e78335a471dbe8a45ba57ce35
Malte Teichert <malte@malteteichert.de>
2023-12-17 19:43:13 +0100

    finish sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
+	import Work from './../lib/icons/Work.svelte';
+	import Person from './../lib/icons/Person.svelte';
+	import Chip from './../lib/icons/Chip.svelte';
+	import { page } from '$app/stores';
+	
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell, AppRail, AppRailAnchor, AppRailTile } from '@skeletonlabs/skeleton';
 
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
@@ -10,6 +15,7 @@
 	import css from 'highlight.js/lib/languages/css';
 	import javascript from 'highlight.js/lib/languages/javascript';
 	import typescript from 'highlight.js/lib/languages/typescript';
+
 
 	hljs.registerLanguage('xml', xml); // for HTML
 	hljs.registerLanguage('css', css);
@@ -21,43 +27,48 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	$: home = $page.url.pathname === '/';
+	$: tech = $page.url.pathname === '/tech';
+	$: work = $page.url.pathname === '/work';
 </script>
 
 <!-- App Shell -->
 <AppShell>
-	<svelte:fragment slot="header">
+	<svelte:fragment slot="sidebarLeft">
 		<!-- App Bar -->
-		<AppBar>
+		<AppRail>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<AppRailAnchor href="/">
+					<span class="Uppercase">Malte Teichert</span>
+				</AppRailAnchor>
 			</svelte:fragment>
+			<!-- --- -->
+			<AppRailAnchor href="/" title="About Me" selected={home}>
+				<svelte:fragment slot="lead">
+					<Person classList="w-6 h-6 mx-auto" />
+				</svelte:fragment>
+				About Me
+			</AppRailAnchor>
+			<AppRailAnchor href="/tech" title="tech-stack" selected={tech}>
+				<svelte:fragment slot="lead">
+					<Chip classList="w-6 h-6 mx-auto" />
+				</svelte:fragment>
+				<span>Tech Stack</span>
+			</AppRailAnchor>
+			<AppRailAnchor href="/work" title="work" selected={work}>
+				<svelte:fragment slot="lead">
+					<Work classList="w-6 h-6 mx-auto" />
+				</svelte:fragment>
+				<span>Work</span>
+			</AppRailAnchor>
+			<!-- --- -->
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<AppRailAnchor href="/contact" title="Contact">
+					<div class="anchor">Contact Me</div>
+				</AppRailAnchor>
 			</svelte:fragment>
-		</AppBar>
+		</AppRail>
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<slot />
